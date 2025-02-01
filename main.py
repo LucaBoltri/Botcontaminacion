8dd2d0f5eed014e9524458e3c97db64622c61add
Lucaboltri <lucaboltrgd@gmail.com>
2025-02-01 16:49:38 -0300

    eliminar token del archivo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
